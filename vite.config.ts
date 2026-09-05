a0350a9a9694c6507cc9325e3ed8e33dd78237b3
mohammed5ibrahim5 <hjgjckmno@gmail.com>
2026-09-05 21:44:24 +0300

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fish/',
+  base: '/',
   server: {
     port: 8080,
     strictPort: true,
